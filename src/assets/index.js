1b6e03eba03e830df0a5395b210b0f0b582106a2
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-08-04 14:36:22 +0530

    changes

---
+++ src/assets/index.js
@@ -0,0 +1,104 @@
+import logo from "./logo.png";
+import backend from "./backend.webp";
+import creator from "./creator.webp";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import CV_Duncan from "./CV_Duncan.png"
+
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import blender from  "./tech/blender.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.webp";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./project/carrent.png";
+import tripguide from "./project/tripguide.png";
+import shoppingcart from "./project/shoppingcart.png";
+import studynotion from "./project/studynotion.png";
+import image11 from "./project/image11.png";
+import studynotion1 from "./project/studynotion1.png"
+import nexuvents from "./project/nexuvents.png"
+import evovleUx from "./project/evovleUx.png"
+import pinterest from "./project/pinterest.png"
+import portfolio from "./project/portfolio.png"
+import shareModal from "./project/shareModal.png"
+import githubFetch from "./project/githubFetch.png"
+import bioPage from "./project/bioPage.png"
+import cuberto from "./project/cuberto.png"
+import twoGood from "./project/twoGood.png"
+import coming from "./project/coming.jpeg"
+import holophin from "./holophin.png"
+import gitmap from "./gitmap.png"
+
+import webdev from "./certificates/webdev.png";
+import certificate1 from "./certificates/certificate1.png";
+import fitt from "./certificates/fitt.png";
+
+export {
+  logo,
+  CV_Duncan,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+    tripguide,
+  blender,
+  shoppingcart,
+  studynotion,
+  image11,
+  webdev,
+  certificate1,
+  fitt,
+  studynotion1,
+evovleUx,
+nexuvents,
+pinterest,
+portfolio,
+shareModal,
+bioPage,
+githubFetch,
+cuberto,
+twoGood,
+coming,
+holophin,
+gitmap,
+};
